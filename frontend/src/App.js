d020c12143974edb35db732883b26b9e0533c408
YosefAdPro <yosef@adpronet.com>
2022-09-05 00:36:56 +0300

    Change main color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
         },
       },
       palette: {
-        primary: { main: "#d500f9" },
+        primary: { main: "#2576d2" },
         secondary: { main: "#F50057" },
       },
       backgroundImage: `url(${lightBackground})`,
