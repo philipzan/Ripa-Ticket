af83e7f4373c605fc2e969c6b4bf8d6b6311950d
YosefAdPro <yosef@adpronet.com>
2023-03-28 15:49:56 +0300

    port + TZ

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,11 +6,12 @@ module.exports = {
     collate: "utf8mb4_bin"
   },
   dialect: process.env.DB_DIALECT || "mysql",
-  timezone: process.env.DB_TIMEZONE || "-03:00",
+  timezone: process.env.DB_TIMEZONE || "+03:00",
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
   logging: false,
   seederStorage: "json",
   seederStoragePath: "sequelizeData.json"
