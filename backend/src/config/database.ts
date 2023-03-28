783933400a500036c5ddc266546b140cc67ebe45
YosefAdPro <yosef@adpronet.com>
2023-03-28 15:53:02 +0300

    port

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -11,7 +11,6 @@ module.exports = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
   logging: false,
   seederStorage: "json",
   seederStoragePath: "sequelizeData.json"
